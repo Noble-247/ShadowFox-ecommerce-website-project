d63d6420bdf5be7a126d4d02ca8a9a868b5bd8c4
Emmanuel Ekpobimi <noble4special247@gmail.com>
2025-02-19 14:38:27 +0100

    Fix grid layout in Cart component for better responsiveness and correct typo in class name

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -59,7 +59,7 @@ const Cart = () => {
           return (
             <div
               key={index}
-              className='py-4 border-t text-gray-700 grid grid-cols-[4fr_0,5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'
+              className='py-4 border-t text-gray-700 grid grid-cols-1 sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'
             >
               <div className='flex items-start gap-6'>
                 <img
@@ -71,7 +71,7 @@ const Cart = () => {
                   <p className='text-xs sm:text-lg font-medium'>
                     {productData.name}
                   </p>
-                  <div className='fles items-center gap-5 mt-2'>
+                  <div className='flex items-center gap-5 mt-2'>
                     <p className='text-xs sm:text-lg font-medium'>
                       {currency}
                       {productData.price}
